67dc7b565ccf5f8a162d32c0d65ada04098d71bc
krompa13 <70905631+krompa13@users.noreply.github.com>
2024-04-10 15:42:36 +0200

    Delete src directory

--- src/main/java/hu/aut/android/kotlinpersonlist/touch/PlayerTouchHelperAdapter.kt
+++
@@ -1,8 +0,0 @@
-package hu.aut.android.kotlinpersonlist.touch
-
-interface PlayerTouchHelperAdapter {
-
-        fun onItemDismissed(position: Int)
-
-        fun onItemMoved(fromPosition: Int, toPosition: Int)
-}
